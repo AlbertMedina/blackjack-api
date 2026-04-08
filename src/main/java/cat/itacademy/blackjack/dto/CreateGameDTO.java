c4a875b355832735e7013280341e329aea3d83c1
Albert Medina <albertmecu@gmail.com>
2026-04-08 11:58:14 +0200

    refactor: remove redundant @NotNull annotations from DTOs

--- src/main/java/cat/itacademy/blackjack/dto/CreateGameDTO.java
+++ src/main/java/cat/itacademy/blackjack/dto/CreateGameDTO.java
@@ -1,9 +1,8 @@
 package cat.itacademy.blackjack.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 public record CreateGameDTO(
-        @NotNull @NotBlank String playerName
+        @NotBlank String playerName
 ) {
 }
--- src/main/java/cat/itacademy/blackjack/dto/UpdatePlayerDTO.java
+++ src/main/java/cat/itacademy/blackjack/dto/UpdatePlayerDTO.java
@@ -1,9 +1,8 @@
 package cat.itacademy.blackjack.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 public record UpdatePlayerDTO(
-        @NotNull @NotBlank String name
+        @NotBlank String name
 ) {
 }
